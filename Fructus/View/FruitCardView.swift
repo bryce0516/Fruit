4f5a06ed761a00a82f13b0a811364f6de8b20d99
hyesungpark <jayhspark@gmail.com>
2023-08-30 20:58:48 +0900

    add card

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,67 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Hye Sung Park on 2023/08/30.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+  // MARK: - PROPERTIES
+  
+  // MARK: - BODY
+  
+  
+  var body: some View {
+    ZStack {
+      VStack(spacing: 20) {
+        // FRUIT: IMAGE
+        Image("blueberry")
+          .resizable()
+          .scaledToFit()
+          .shadow(
+            color: Color(red: 0, green: 0, blue:0, opacity: 0.15), radius: 8,
+            x: 6,
+            y: 8
+          )
+        // FRUIT: TITLE
+        
+        Text("Blueberry")
+          .foregroundColor(Color.white)
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .shadow(
+            color: Color(red:0, green: 0, blue:0, opacity: 0.15), radius: 2, x:2, y: 2
+          )
+        // FRUIT: HEADLINE
+        
+        // BUTTON: START
+      }
+    } //: VSTACK
+    .frame(
+      minWidth: 0,
+      maxWidth: .infinity,
+      minHeight: 0,
+      maxHeight: .infinity,
+      alignment: .center
+    )
+    .background(
+      LinearGradient(
+        gradient: Gradient(
+          colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]),
+        startPoint: .top,
+        endPoint: .bottom
+      )
+    )
+    .cornerRadius(20)
+  }
+  
+}
+// MARK: - PREVIEW
+struct FruitCardView_Previews: PreviewProvider {
+  static var previews: some View {
+    FruitCardView()
+      .previewLayout(.fixed(width: 320, height: 640))
+  }
+}
