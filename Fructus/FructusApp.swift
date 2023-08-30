08a47d2d8438f4f835207c215f44579aa4a38ad4
hyesungpark <jayhspark@gmail.com>
2023-08-30 20:21:39 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Hye Sung Park on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
