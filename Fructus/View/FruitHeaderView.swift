509baa4fe2f91fe4c5411aa6f526748b8d2f280a
hyesungpark <jayhspark@gmail.com>
2023-09-06 23:47:00 +0900

    add header view

--- Fructus/View/FruitHeaderView.swift
+++ Fructus/View/FruitHeaderView.swift
@@ -12,14 +12,37 @@ struct FruitHeaderView: View {
   
   var fruit: Fruit
   
+  @State private var isAnimatingImage: Bool = false
+  
   // MARK: - BODY
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    ZStack {
+      LinearGradient(
+        gradient: Gradient(
+          colors: fruit.gradientColors),
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+      )
+      Image(fruit.image)
+        .resizable()
+        .scaledToFit()
+        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6,y: 8)
+        .padding(.vertical, 20)
+        .scaleEffect(isAnimatingImage ? 1.0 : 0.6)
+      
+    } //: ZSTACK
+    .frame(height: 440)
+    .onAppear(){
+      withAnimation(.easeOut(duration: 0.5)) {
+        isAnimatingImage = true
+      }
     }
+  }
 }
 
 struct FruitHeaderView_Previews: PreviewProvider {
-    static var previews: some View {
-        FruitHeaderView(fruit: fruitsData[0])
-    }
+  static var previews: some View {
+    FruitHeaderView(fruit: fruitsData[0])
+      .previewLayout(.fixed(width: 375, height: 440))
+  }
 }
